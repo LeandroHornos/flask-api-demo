aa14ae5e823ee8f90d468a257e0b9926291efeab
LeanderShulgin <leandrohornos@gmail.com>
2022-02-20 10:37:40 -0300

    Added post request to create new user

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 from jinja2 import Undefined
@@ -36,7 +36,27 @@ def userById(user_id):
     return jsonify(user)
 
 
+@app.route("/addnewuser", methods=["POST"])
+def addNewUser():
+    # Get the last id and use the next number as the new id
+    sorted_data = sorted(data, key=lambda d: d['id'])
+    new_id = sorted_data[-1]["id"] + 1
+    # Get user data from request
+    # Some validation should be done here
+    new_user = request.json
+    # add new id and save to "database"
+    new_user["id"] = new_id
+    data.append(new_user)
+    with open('mock-users.json', 'w') as outfile:
+        outfile.write(json.dumps(data))
+
+    # Update users list
+
+    return jsonify({"message": "success", "user": new_user})
+
 # Helper functions
+
+
 def getUserById(id, users):
     print("id:", id)
     match = {"user": Undefined, "message": "not found"}
